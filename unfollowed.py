1d83f0bb34e4bed91c6cdd7430e294136441952d
cxvris <157775002+cxvris@users.noreply.github.com>
2024-02-06 15:32:37 -0800

    Add files via upload

---
+++ unfollowed.py
@@ -0,0 +1,25 @@
+from whosgone.utils import IGExtractor
+from whosgone.core.output import TextOutput, HTMLOutput
+from whosgone.core import settings
+import os
+
+if __name__ == "__main__":
+    settings.initialize()
+
+    zip_path = input("Please enter the file path for your instagram data file: ")
+    while not os.path.exists(zip_path) or not zip_path.endswith(".zip"):
+        zip_path = input("Input was invalid, please try again: ")
+
+    igextract = IGExtractor(zip_path)
+
+    followers = igextract.get_followers()
+    following = igextract.get_following()
+
+    output = None
+        
+    if settings.use_html():
+        output = HTMLOutput(followers, following)
+    else:
+        output = TextOutput(followers, following)
+
+    output.create_output()
